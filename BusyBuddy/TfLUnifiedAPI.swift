797138d481a8937a8758e8eca323f222fb1bcc20
Ralf Michael Yap <ralfvyap@gmail.com>
2020-11-02 10:04:11 +0000

    API key now an environment variable.

---
+++ BusyBuddy/TfLUnifiedAPI.swift
@@ -0,0 +1,38 @@
+//
+//  TfLUnifiedAPI.swift
+//  BusyBuddy
+//
+//  Created by Ralf Michael Yap on 31/10/2020.
+//
+
+import Foundation
+import UIKit
+
+struct TfLUnifiedAPI {
+    
+    private let session: URLSession
+    
+    let prefix = "https://api.tfl.gov.uk/"
+    let api_key = ProcessInfo.processInfo.environment["api_key"]
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func fetchAllJamCams(completion: @escaping (Result<[Place], Error>) -> Void) {
+        guard let url = URL(string: self.prefix + "Place/Type/JamCam?app_key=" + self.api_key!) else { return }
+        
+        session.dataTask(with: url) { (data, response, error) in
+            if let err = error {
+                completion(.failure(err))
+            }
+                        
+            do {
+                let places = try JSONDecoder().decode([Place].self, from: data!)
+                completion(.success(places))
+            } catch let jsonError {
+                completion(.failure(jsonError))
+            }
+        }.resume()
+    }
+}
