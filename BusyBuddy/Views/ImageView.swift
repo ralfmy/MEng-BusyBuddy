3585a3b5f4f71a30fecec2cddb0d5066eaffc4fc
Ralf Michael Yap <ralfvyap@gmail.com>
2020-12-02 16:36:44 +0000

    Adjusted colours; Added ability to view image within Place Detail.

---
+++ BusyBuddy/Views/ImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageView.swift
+//  BusyBuddy
+//
+//  Created by Ralf Michael Yap on 02/12/2020.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
