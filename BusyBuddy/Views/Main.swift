1fcbc72424f510dc68789366d9c16f46565a5fa4
Ralf Michael Yap <ralfvyap@gmail.com>
2021-03-10 11:22:01 +0000

    Reload Bookmarks scores on scene active.

--- BusyBuddy/Views/Main.swift
+++ BusyBuddy/Views/Main.swift
@@ -7,17 +7,28 @@
 //  With help from:
 //  https://peterfriese.dev/ultimate-guide-to-swiftui2-application-lifecycle/
 //  https://swiftwithmajid.com/2020/08/19/managing-app-in-swiftui/
+//  https://swiftwithmajid.com/2020/08/26/managing-scenes-in-swiftui/
 
 import SwiftUI
 
 @main
 struct BusyBuddy: App {
+    @Environment(\.scenePhase) var scenePhase
+
     @StateObject var appState: AppState = AppState()
     @StateObject var placesModel: PlacesModel = PlacesModel()
     
     var body: some Scene {
         WindowGroup {
             AppView().environmentObject(appState).environmentObject(placesModel)
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                self.placesModel.updateBookmarksScores()
+            default:
+                break;
+            }
         }
     }
 }
